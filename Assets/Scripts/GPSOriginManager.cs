5505bf434d37fad1d7c06987e0b15c81488238b5
Adam Wittmann <adamjwittmann@gmail.com>
2025-06-18 19:02:00 -0400

    maybe solved the origin spawning, idk gonna go outside and see... So much debugging added, gonna double check logic before next commit

--- Assets/Scripts/GPSOriginManager.cs
+++ Assets/Scripts/GPSOriginManager.cs
@@ -15,17 +15,54 @@ public class GPSOriginManager : MonoBehaviour
     // Declare public variables to store origin latitude, longitude, and altitude
     public double originLatitude = 41.721247;
     public double originLongitude = -73.932518;
+    
+    //Testing with spawning at the opposite side of basement of DN
+    // public double originLatitude = 41.72081;
+    // public double originLongitude = -73.9328304;
     public double originAltitude = 30.0;
     // Optional: define a constant for Earth's radius in meters
     private const double earthRadius = 6378137;
 
     public GameObject originPrefab;
-    void Awake()
+    public GameObject sceneRoot;
+    void Start()
     {
-        Vector3 originPos = ConvertToUnityPosition(originLatitude, originLongitude, originAltitude);
-        Instantiate(originPrefab, originPos, Quaternion.identity);
-
+        sceneRoot ??= this.gameObject;
+        StartCoroutine(PlaceOriginWhenReady());
+        
     }
+        IEnumerator PlaceOriginWhenReady()
+        {
+        while (!LocationManager.Instance.IsReady)
+    {
+        yield return null;
+    }
+
+    if (sceneRoot == null)
+    {
+        Debug.LogError("sceneRoot is not assigned!");
+        yield break;
+    }
+
+    if (originPrefab == null)
+    {
+        Debug.LogError("originPrefab is not assigned!");
+        yield break;
+    }
+
+    Debug.Log("LocationManager is ready. Proceeding to place origin.");
+            Vector3 originPos = ConvertToUnityPosition(originLatitude, originLongitude, originAltitude);
+            Vector3 devicePos = ConvertToUnityPosition(LocationManager.Instance.deviceLat, LocationManager.Instance.deviceLong, LocationManager.Instance.deviceAlt);
+            Vector3 offset = devicePos - originPos;
+            sceneRoot.transform.position = -offset;
+            Debug.Log($"Origin: ({originLatitude}, {originLongitude}, {originAltitude})");
+            Debug.Log($"Device: ({LocationManager.Instance.deviceLat}, {LocationManager.Instance.deviceLong}, {LocationManager.Instance.deviceAlt})");
+            Debug.Log($"OriginPos: {originPos}, DevicePos: {devicePos}, Offset: {offset}");
+            Instantiate(originPrefab, -offset, Quaternion.identity, sceneRoot.transform);
+            //test
+            Vector3 placePos = new Vector3(0, 0, 10); // 10 meters forward in Unity space
+            Instantiate(originPrefab, placePos, Quaternion.identity, sceneRoot.transform);
+        }
     public Vector3 ConvertToUnityPosition(double targetLat, double targetLon, double targetAlt)
     {
         // Convert degrees to radians for math
--- Assets/Scripts/LocationManager.cs
+++ Assets/Scripts/LocationManager.cs
@@ -1,9 +1,15 @@
 // LocationManager.cs
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 public class LocationManager : MonoBehaviour
 {
     public static LocationManager Instance;
+
+    public double deviceLat;
+    public double deviceLong;
+    public double deviceAlt;
+    public bool IsReady { get; private set; } = false;
 
     void Awake()
     {
@@ -15,24 +21,47 @@ public class LocationManager : MonoBehaviour
         }
     }
 
-    public LocationInfo GetCurrentLocation()
+    private void Start()
     {
-        return Input.location.lastData;
+        StartCoroutine(StartLocationService());
     }
 
-    IEnumerator Start()
+    private IEnumerator StartLocationService()
     {
         if (!Input.location.isEnabledByUser)
+        {
+            Debug.LogWarning("GPS not enabled");
             yield break;
+        }
 
         Input.location.Start();
 
-        while (Input.location.status == LocationServiceStatus.Initializing)
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
             yield return new WaitForSeconds(1);
+            maxWait--;
+        }
 
-        if (Input.location.status == LocationServiceStatus.Running)
-            Debug.Log("Location service started");
-        else
-            Debug.Log("Location service failed");
+        if (maxWait <= 0)
+        {
+            Debug.LogError("Timed out while initializing GPS.");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.LogError("Unable to determine device location.");
+            yield break;
+        }
+
+        var data = Input.location.lastData;
+        deviceLat = data.latitude;
+        deviceLong = data.longitude;
+        deviceAlt = data.altitude;
+
+        Debug.Log($"GPS Ready: Lat {deviceLat}, Lon {deviceLong}, Alt {deviceAlt}");
+
+        IsReady = true;
     }
 }
